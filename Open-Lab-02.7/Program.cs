c6ea235e787f60d301ad500c964f8223d8e8f21d
samuel zaťovič <szatovic@gmail.com>
2019-11-02 17:21:42 +0100

    Hotový program

---
+++ Open-Lab-02.7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Open_Lab_02._7
+{
+    class Program
+    {
+
+        public static bool IsTimeForMilkAndCookies(int year, int month, int day)
+        {
+          if (month == 12 && day == 24);
+                return true;
+        }
+    }
+}
